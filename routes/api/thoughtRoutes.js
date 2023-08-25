41d91ebee58d3cb4f1153df6fc57b800b44d8172
Engine <rg.dartigue@gmail.com>
2023-08-24 21:07:34 -0400

    small tweeks to thoughtRoute

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,15 +10,10 @@ const {
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
-router.route('/')
-        .get(getThoughts)
-        .post(createThought);
+router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId')
-        .get(getSingleThought)
-        .put(updateThought)
-        .delete(deleteThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 router.route('/:thoughtId/reactions').post(addThoughtReaction);
 
